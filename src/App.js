82cc5bc9403d38b252cb06b973ca18a42d2329fc
Jurijs Belozjorovs <Juriy@btv.lv>
2021-11-11 15:59:45 +0200

    kkads message

--- src/App.js
+++ src/App.js
@@ -1,75 +1,108 @@
-import { useState } from "react";
-import ShopItems from "./components/ShopItems/ShopItems";
-import Button from "./components/Button/Button";
-import SearchField from "./components/SearchField/SearchField";
-import ShopBasket from "./components/ShopBasket/ShopBasket";
+import { useState, useEffect } from 'react';
+import ShopItems from './components/ShopItems/ShopItems';
+import Button from './components/Button/Button';
+import SearchField from './components/SearchField/SearchField';
+import ShopBasket from './components/ShopBasket/ShopBasket';
+import Heading from './components/Heading/Heading';
 
 const basket = [0, 0];
-const buttons = ["All", "Cakes", "Cupcakes", "Sweets", "Doughnuts"];
+const buttons = ['All', 'Cakes', 'Cupcakes', 'Sweets', 'Doughnuts'];
 const shopItemObject = [
   {
-    title: "Sweet item",
+    title: 'Sweet item',
     price: 5,
-    imgSrc: "https://picsum.photos/250",
-    type: "sweets",
+    imgSrc: 'https://picsum.photos/250',
+    type: 'sweets',
   },
   {
-    title: "Cupcake",
+    title: 'Cupcake',
     price: 7,
-    imgSrc: "https://picsum.photos/250?random=1",
-    type: "cupcakes",
+    imgSrc: 'https://picsum.photos/250?random=1',
+    type: 'cupcakes',
   },
   {
-    title: "Doughnut",
+    title: 'Doughnut',
     price: 10,
-    imgSrc: "https://picsum.photos/250?random=2",
-    type: "doughnuts",
+    imgSrc: 'https://picsum.photos/250?random=2',
+    type: 'doughnuts',
   },
   {
-    title: "Cake",
+    title: 'Cake',
     price: 3,
-    imgSrc: "https://picsum.photos/250?random=3",
-    type: "cakes",
+    imgSrc: 'https://picsum.photos/250?random=3',
+    type: 'cakes',
   },
   {
-    title: "Very sweet item",
+    title: 'Very sweet item',
     price: 6,
-    imgSrc: "https://picsum.photos/250?random=4",
-    type: "sweets",
+    imgSrc: 'https://picsum.photos/250?random=4',
+    type: 'sweets',
   },
   {
-    title: "Chocolate",
+    title: 'Chocolate',
     price: 4,
-    imgSrc: "https://picsum.photos/250?random=5",
-    type: "cakes",
+    imgSrc: 'https://picsum.photos/250?random=5',
+    type: 'cakes',
   },
   {
-    title: "Muffin",
+    title: 'Muffin',
     price: 2,
-    imgSrc: "https://picsum.photos/250?random=6",
-    type: "cupcakes",
+    imgSrc: 'https://picsum.photos/250?random=6',
+    type: 'cupcakes',
   },
   {
-    title: "Halva",
+    title: 'Halva',
     price: 8,
-    imgSrc: "https://picsum.photos/250?random=7",
-    type: "sweets",
+    imgSrc: 'https://picsum.photos/250?random=7',
+    type: 'sweets',
   },
   {
-    title: "Pudding",
+    title: 'Pudding',
     price: 1,
-    imgSrc: "https://picsum.photos/250?random=8",
-    type: "doughnuts",
+    imgSrc: 'https://picsum.photos/250?random=8',
+    type: 'doughnuts',
   },
 ];
 
 const App = () => {
   const [shopItems, setShopItems] = useState(shopItemObject);
-  const [search, setSearch] = useState("");
+  const [search, setSearch] = useState('');
   const [shopBasket, setShopBasket] = useState(basket);
+  const [counter, setCounter] = useState(0);
+  const colorHeading = ['blue', 'red', 'green', 'yellow', 'black', 'pink'];
+
+  useEffect(() => {
+    alert('Please, click "Ok" button');
+  }, []);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setCounter(counter + 1);
+    }, 1000);
+  });
+
+  useEffect(() => {
+    const clickButtons = document.querySelectorAll('.button');
+    clickButtons.forEach((button) => {
+      button.addEventListener('click', () => {
+        document.querySelector('h1').style.color =
+          colorHeading[Math.floor(Math.random() * colorHeading.length)];
+      });
+    });
+  });
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      if (counter > 9) {
+        document.querySelector('h1').style.color =
+          colorHeading[Math.floor(Math.random() * colorHeading.length)];
+      }
+    }, 1000);
+    return () => clearTimeout(timeout);
+  });
 
   const shopFilter = (value) => {
-    if (value.toLowerCase() === "all") {
+    if (value.toLowerCase() === 'all') {
       setShopItems(shopItemObject);
     } else {
       const filtredShopItems = [...shopItemObject].filter((item) => {
@@ -81,6 +114,7 @@ const App = () => {
 
   return (
     <>
+      <Heading counter={counter} />
       <ShopBasket totalItems={shopBasket[0]} totalPrice={shopBasket[1]} />
       <h1 className="heading">
         Our <span className="heading--pink">Store</span>
---
+++ src/components/Heading/Heading.js
@@ -0,0 +1,10 @@
+const Heading = ({ counter }) => {
+  return (
+    <h1>
+      "Priecīgus ziemassvētkus!"
+      <br /> Jūs atradāties šajā lapā {counter} sekundes.
+    </h1>
+  );
+};
+
+export default Heading;
